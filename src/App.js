e3448a7c1d246fa57d3c55e381b899cbdcc12f6a
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-04-21 11:54:31 +0100

    implemented react-route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import ProductList from "./components/ProductList";
+import ProductDetails from "./ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <ProductList />
+      <Switch>
+        <Route exact path="/product" component={ProductList} />
+        <Route path="/productdetails/:id" component={ProductDetails} />
+        <Redirect to="/product" />
+      </Switch>
     </div>
   );
 }
